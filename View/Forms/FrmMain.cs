62cc0c54a69e6b17a3c721f5bf8475ece6f6807c
Paulo Jirón <kuroko0010@gmail.com>
2023-03-20 00:01:52 -0600

    Add project files.

---
+++ View/Forms/FrmMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using View.Util;
+
+namespace View.Forms
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            dtgData.DataSource = null;
+
+            var data = AproximateMethods.BisectionMethod
+                                          (
+                                          txtFunction.Text, 
+                                          Double.Parse(txtXa.Text), 
+                                          Double.Parse(txtXb.Text), 
+                                          0.003d
+                                          );
+
+            dtgData.DataSource = data;
+        }
+    }
+}
---
+++ View/Models/Bisection.cs
@@ -0,0 +1,13 @@
+﻿namespace View.Models;
+
+public class Bisection
+{
+    public int I { get; set; }
+    public double Xa { get; set; }
+    public double Xb { get; set; }
+    public double Xr => (Xa + Xb) / 2;
+    public double Fxa { get; set; }
+    public double Fxr { get; set; }
+    public double FxaFr => Fxa * Fxr;
+    public double Ea { get; set; }
+}
